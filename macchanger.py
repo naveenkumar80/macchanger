308ae27c87679f54f90b5a53c386637ab2b92a27
Naveen kumar <108688469+naveenkumar80@users.noreply.github.com>
2022-08-23 11:49:01 +0530

    Add files via upload

--- macchanger.py
+++ macchanger.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 import subprocess
 import optparse
 import re
+
 
 def get_argument():
     parser = optparse.OptionParser()
@@ -16,12 +17,6 @@ def get_argument():
 
 
 def change_mac(interface, new_mac):
-    # interface = input("enter the interface = ")
-    # new_mac = input("enter the new mac address = ")
-    # subprocess.call("ifconfig "+interface+" down", shell=True)
-    # subprocess.call("ifconfig "+interface+" hw ether "+new_mac, shell=True)
-    # subprocess.call("ifconfig "+interface+" up", shell=True)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
@@ -29,19 +24,17 @@ def change_mac(interface, new_mac):
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    # print(ifconfig_result)
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
         print("[+] could not read MAC address.")
 
-
-# interface = options.interface
-# new_mac = options.new_mac
 options = get_argument()
 current_mac = get_current_mac(options.interface)
 print("Current_MAC = " + str(current_mac))
+
 change_mac(options.interface, options.new_mac)
 
 current_mac = get_current_mac(options.interface)
@@ -49,4 +42,3 @@ if current_mac == options.new_mac:
     print("[+] MAC address was successfully changed to " + current_mac)
 else:
     print("[-] MAC address did no get changed.")
-
